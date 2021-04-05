6921d892c54cf871c2c9a317d7460496d27c19b2
tora_pan <travis.pandos@gmail.com>
2021-04-04 20:27:12 -0700

    update .gitignore

--- API/Startup.cs
+++ API/Startup.cs
@@ -55,7 +55,7 @@ namespace API
 
             app.UseRouting();
             // allow CORS coming from the Angular app.
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
