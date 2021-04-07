58a149af56041c5af533d514a785817e0b2f33fe
tora_pan <travis.pandos@gmail.com>
2021-04-06 21:45:25 -0700

    add toekn service, create token logic

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Services;
+using API.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,6 +35,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddScoped<ITokenService, TokenService>();
             services.AddControllers();
             services.AddCors();
             services.AddSwaggerGen(c =>
