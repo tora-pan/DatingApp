b6556ad0c0a0075329e01d97ae8895d7c67ee432
tora_pan <travis.pandos@gmail.com>
2021-04-06 20:55:48 -0700

    add a login endpoint and fix capitalization login bug

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.DTOs
 {
-    public class RegisterDto
+    public class LoginDto
     {
-        [Required]  // this makes the fields required making sure the user types something in.
         public string Username { get; set; }
-        [Required]
         public string Password { get; set; }
     }
 }
