43d97c17d9b8d549dd91bf5d92f691edcd9b0d17
tora_pan <travis.pandos@gmail.com>
2021-04-21 21:15:48 -0700

    start working on client ANGULAR

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Local } from 'protractor/built/driverProviders';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
