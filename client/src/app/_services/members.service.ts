75d94da1e679e0c9922c75920c44ee245b902cd7
tora_pan <travis.pandos@gmail.com>
2021-04-22 20:55:11 -0700

    add member card, add interceptor to pass jwt with request

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 
@@ -11,19 +10,13 @@ import { Member } from '../_models/member';
 export class MembersService {
   baseUrl = environment.apiUrl;
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-    })
-  }
-
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
